3d4c88b9b1890bddce1afda63a8a5f7bfca281fd
Ali Hadwan <alihadwan@Alis-MacBook-Pro.local>
2016-01-24 21:32:24 -0800

    edit MovieViewer.xcworkspace

--- MovieViewer/MoviesViewController.swift
+++ MovieViewer/MoviesViewController.swift
@@ -1,29 +1,100 @@
 //
-//  MoviesViewController.swift
+//  MovieViewController.swift
 //  MovieViewer
 //
-//  Created by Ali Hadwan on 1/16/16.
+//  Created by Ali Hadwan on 1/22/16.
 //  Copyright © 2016 Ali Hadwan. All rights reserved.
 //
 
 import UIKit
 import AFNetworking
+import BXProgressHUD
 
-class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate{
+
     @IBOutlet weak var tableView: UITableView!
-    var movies:[NSDictionary]?
     
-
+    @IBOutlet weak var searchBar: UISearchBar!
+    
+    var movies: [NSDictionary]?
+    let refreshControl = UIRefreshControl()
+    var movieData: [NSDictionary]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        refreshControl.backgroundColor = UIColor.blackColor()
+        refreshControl.tintColor = UIColor.whiteColor()
+        
+        
+        searchBar.delegate = self
+    
+        
+        // Initialize a UIRefreshControl
+        //let refreshControl = UIRefreshControl()
+        refreshControl.addTarget(self, action: "refreshControlAction:", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.insertSubview(refreshControl, atIndex: 0)
+
+        
         tableView.dataSource = self
         tableView.delegate = self
+        movieData = movies
+        
+        var targetView: UIView {
+            
+             return self.view
+            
+        }
+        
         
         let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
-        let url = NSURL(string:"https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let url = NSURL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
         let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate: nil,
+            delegateQueue: NSOperationQueue.mainQueue()
+        )
+        
+        BXProgressHUD.showHUDAddedTo(targetView).hide(afterDelay: 3)
+        
+        let task: NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            print("response: \(responseDictionary)")
+                            
+                            self.movies = responseDictionary["results"] as! [NSDictionary]
+                          self.movieData = self.movies
+                        self.tableView.reloadData()
+                            self.refreshControl.endRefreshing()
+                        
+                    }
+                }
+        })
+        task.resume()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func tap(sender: AnyObject) {
+        
+        view.endEditing(true)
+    }
+    
+    // Makes a network request to get updated data
+    // Updates the tableView with the new data
+    // Hides the RefreshControl
+    func refreshControlAction(refreshControl: UIRefreshControl) {
+        let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+        let url = NSURL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)")
+        let request = NSURLRequest(URL: url!)
+        // ... Create the NSURLRequest (myRequest) ...
+        
+        // Configure session so that completion handler is executed on main UI thread
         let session = NSURLSession(
             configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
             delegate:nil,
@@ -31,51 +102,64 @@ class MoviesViewController: UIViewController, UITableViewDelegate, UITableViewDa
         )
         
         let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
-            completionHandler: { (dataOrNil, response, error) in
-                if let data = dataOrNil {
-                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
-                        data, options:[]) as? NSDictionary {
-                            NSLog("response: \(responseDictionary)")
-                            
-                            self.movies = responseDictionary["results"] as! [NSDictionary]
-                            self.tableView.reloadData()
-                            
-                    }
-                }
+            completionHandler: { (data, response, error) in
+                
+                // ... Use the new data to update the data source ...
+                
+                // Reload the tableView now that there is new data
+                self.tableView.reloadData()
+                
+                // Tell the refreshControl to stop spinning
+                refreshControl.endRefreshing()	
         });
         task.resume()
-
-        // Do any additional setup after loading the view.
     }
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        movieData = searchText.isEmpty ? movies : movies!.filter({ (movie: NSDictionary) -> Bool in
+        return (movie["title"] as! String).rangeOfString(searchText, options: .CaseInsensitiveSearch) != nil
+        })
+        self.tableView.reloadData()
+    }
+    
+    
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        
+        if let movieData = movieData{
+            return movieData.count
+       }else{
+        return 0
+    }
+}
+    
+        func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+            
+            let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
+            //let movie = movies![indexPath.row]
+            let movie = movieData![indexPath.row]
+            let title = movie ["title"] as! String
+            let overview = movie ["overview"] as! String
+            let posterpath = movie ["poster_path"] as! String
+            let baseUrl = "http://image.tmdb.org/t/p/w500"
+            let imageUrl = NSURL(string: baseUrl + posterpath)
+            
+            
+            cell.titleLabel!.text = title
+            cell.overviewLabel.text = overview
+            cell.posterView.setImageWithURL(imageUrl!)
+           
+            print("row \(indexPath.row)")
+            return cell
+            
+    }
+
+    
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
-        if let movies = movies{
-            return movies.count
-        }else{
-            return 0
-        }
-    }
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
-        let cell = tableView.dequeueReusableCellWithIdentifier("MovieCell", forIndexPath: indexPath) as! MovieCell
-        let movie = movies![indexPath.row]
-        let title = movie["title"] as! String
-        let overview = movie["overview"] as! String
-        let posterPath = movie["poster_path"] as! String
-        let baseUrl = "http://image.tmdb.org/t/p/w500"
-        let imageUrl = NSURL (string: baseUrl + posterPath)
-
-        cell.posterView.setImageWithURL(imageUrl!)
-        cell.titleLabel.text = title
-        cell.overviewLabel.text = overview
-        
-                print("row \(indexPath.row)")
-        return cell
-    }
-    
     
 
     /*
